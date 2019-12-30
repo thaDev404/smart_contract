be1ebcf2012736b462a64f36ed183d02f3749bdf
thadev404 <oluwatobilobaolusoga@gmail.com>
2019-12-30 05:37:53 -0600

    first commit

---
+++ contracts/inbox.go
@@ -0,0 +1,243 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package contracts
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = abi.U256
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// InboxABI is the input ABI used to generate the binding from.
+const InboxABI = "[{\"constant\":false,\"inputs\":[{\"name\":\"newMessage\",\"type\":\"string\"}],\"name\":\"setMessage\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"message\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"name\":\"initialMessage\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"}]"
+
+// InboxFuncSigs maps the 4-byte function signature to its string representation.
+var InboxFuncSigs = map[string]string{
+	"e21f37ce": "message()",
+	"368b8772": "setMessage(string)",
+}
+
+// InboxBin is the compiled bytecode used for deploying new contracts.
+var InboxBin = "0x6060604052341561000f57600080fd5b60405161038e38038061038e833981016040528080519091019050600081805161003d929160200190610044565b50506100df565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061008557805160ff19168380011785556100b2565b828001600101855582156100b2579182015b828111156100b2578251825591602001919060010190610097565b506100be9291506100c2565b5090565b6100dc91905b808211156100be57600081556001016100c8565b90565b6102a0806100ee6000396000f300606060405263ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663368b87728114610047578063e21f37ce1461009a57600080fd5b341561005257600080fd5b61009860046024813581810190830135806020601f8201819004810201604051908101604052818152929190602084018383808284375094965061012495505050505050565b005b34156100a557600080fd5b6100ad61013b565b60405160208082528190810183818151815260200191508051906020019080838360005b838110156100e95780820151838201526020016100d1565b50505050905090810190601f1680156101165780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b60008180516101379291602001906101d9565b5050565b60008054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156101d15780601f106101a6576101008083540402835291602001916101d1565b820191906000526020600020905b8154815290600101906020018083116101b457829003601f168201915b505050505081565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061021a57805160ff1916838001178555610247565b82800160010185558215610247579182015b8281111561024757825182559160200191906001019061022c565b50610253929150610257565b5090565b61027191905b80821115610253576000815560010161025d565b905600a165627a7a7230582023ed15068cec9e39ecf3575e0458200608b33d94325251e5afa327983f2d1aa20029"
+
+// DeployInbox deploys a new Ethereum contract, binding an instance of Inbox to it.
+func DeployInbox(auth *bind.TransactOpts, backend bind.ContractBackend, initialMessage string) (common.Address, *types.Transaction, *Inbox, error) {
+	parsed, err := abi.JSON(strings.NewReader(InboxABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(InboxBin), backend, initialMessage)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Inbox{InboxCaller: InboxCaller{contract: contract}, InboxTransactor: InboxTransactor{contract: contract}, InboxFilterer: InboxFilterer{contract: contract}}, nil
+}
+
+// Inbox is an auto generated Go binding around an Ethereum contract.
+type Inbox struct {
+	InboxCaller     // Read-only binding to the contract
+	InboxTransactor // Write-only binding to the contract
+	InboxFilterer   // Log filterer for contract events
+}
+
+// InboxCaller is an auto generated read-only Go binding around an Ethereum contract.
+type InboxCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// InboxTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type InboxTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// InboxFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type InboxFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// InboxSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type InboxSession struct {
+	Contract     *Inbox            // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// InboxCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type InboxCallerSession struct {
+	Contract *InboxCaller  // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// InboxTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type InboxTransactorSession struct {
+	Contract     *InboxTransactor  // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// InboxRaw is an auto generated low-level Go binding around an Ethereum contract.
+type InboxRaw struct {
+	Contract *Inbox // Generic contract binding to access the raw methods on
+}
+
+// InboxCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type InboxCallerRaw struct {
+	Contract *InboxCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// InboxTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type InboxTransactorRaw struct {
+	Contract *InboxTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewInbox creates a new instance of Inbox, bound to a specific deployed contract.
+func NewInbox(address common.Address, backend bind.ContractBackend) (*Inbox, error) {
+	contract, err := bindInbox(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Inbox{InboxCaller: InboxCaller{contract: contract}, InboxTransactor: InboxTransactor{contract: contract}, InboxFilterer: InboxFilterer{contract: contract}}, nil
+}
+
+// NewInboxCaller creates a new read-only instance of Inbox, bound to a specific deployed contract.
+func NewInboxCaller(address common.Address, caller bind.ContractCaller) (*InboxCaller, error) {
+	contract, err := bindInbox(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &InboxCaller{contract: contract}, nil
+}
+
+// NewInboxTransactor creates a new write-only instance of Inbox, bound to a specific deployed contract.
+func NewInboxTransactor(address common.Address, transactor bind.ContractTransactor) (*InboxTransactor, error) {
+	contract, err := bindInbox(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &InboxTransactor{contract: contract}, nil
+}
+
+// NewInboxFilterer creates a new log filterer instance of Inbox, bound to a specific deployed contract.
+func NewInboxFilterer(address common.Address, filterer bind.ContractFilterer) (*InboxFilterer, error) {
+	contract, err := bindInbox(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &InboxFilterer{contract: contract}, nil
+}
+
+// bindInbox binds a generic wrapper to an already deployed contract.
+func bindInbox(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(InboxABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Inbox *InboxRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Inbox.Contract.InboxCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Inbox *InboxRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Inbox.Contract.InboxTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Inbox *InboxRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Inbox.Contract.InboxTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Inbox *InboxCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Inbox.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Inbox *InboxTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Inbox.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Inbox *InboxTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Inbox.Contract.contract.Transact(opts, method, params...)
+}
+
+// Message is a free data retrieval call binding the contract method 0xe21f37ce.
+//
+// Solidity: function message() constant returns(string)
+func (_Inbox *InboxCaller) Message(opts *bind.CallOpts) (string, error) {
+	var (
+		ret0 = new(string)
+	)
+	out := ret0
+	err := _Inbox.contract.Call(opts, out, "message")
+	return *ret0, err
+}
+
+// Message is a free data retrieval call binding the contract method 0xe21f37ce.
+//
+// Solidity: function message() constant returns(string)
+func (_Inbox *InboxSession) Message() (string, error) {
+	return _Inbox.Contract.Message(&_Inbox.CallOpts)
+}
+
+// Message is a free data retrieval call binding the contract method 0xe21f37ce.
+//
+// Solidity: function message() constant returns(string)
+func (_Inbox *InboxCallerSession) Message() (string, error) {
+	return _Inbox.Contract.Message(&_Inbox.CallOpts)
+}
+
+// SetMessage is a paid mutator transaction binding the contract method 0x368b8772.
+//
+// Solidity: function setMessage(string newMessage) returns()
+func (_Inbox *InboxTransactor) SetMessage(opts *bind.TransactOpts, newMessage string) (*types.Transaction, error) {
+	return _Inbox.contract.Transact(opts, "setMessage", newMessage)
+}
+
+// SetMessage is a paid mutator transaction binding the contract method 0x368b8772.
+//
+// Solidity: function setMessage(string newMessage) returns()
+func (_Inbox *InboxSession) SetMessage(newMessage string) (*types.Transaction, error) {
+	return _Inbox.Contract.SetMessage(&_Inbox.TransactOpts, newMessage)
+}
+
+// SetMessage is a paid mutator transaction binding the contract method 0x368b8772.
+//
+// Solidity: function setMessage(string newMessage) returns()
+func (_Inbox *InboxTransactorSession) SetMessage(newMessage string) (*types.Transaction, error) {
+	return _Inbox.Contract.SetMessage(&_Inbox.TransactOpts, newMessage)
+}
---
+++ deploy.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	_ "os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/thadev404/smart_contract/contracts"
+)
+
+const key = `{"address":"9e283dd051ab9b8c5053ee746ccf6104d2261d8e","crypto":{"cipher":"aes-128-ctr","ciphertext":"0587d5d4b3802e1aba158189063c4c7c288be009f35c09b30f80363fe15b9c62","cipherparams":{"iv":"46ae07bb9905fba304328723f925571c"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"324e93f9444994dad70f25fca21e0396825da94c2cf23f181fdad5620c22a5b1"},"mac":"3bb7811b5509754ddd2a8b445a70ae71bc87a4873f8f1f67316b7414a5e7e8c0"},"id":"93d20f29-cc09-4a02-9954-a4e18aa2616a","version":3}`
+
+func mains() {
+	// connect to an ethereum node  hosted by infura
+	blockchain, err := ethclient.Dial("https://rinkeby.infura.io/v3/f5a4e8b0989e4ec79ca246113ad7309a")
+
+	if err != nil {
+		log.Fatalf("Unable to connect to network:%v\n", err)
+	}
+
+	// Get credentials for the account to charge for contract deployments
+	auth, err := bind.NewTransactor(strings.NewReader(key), "Jesuskid123,")
+
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+	address, _, _, _ := contracts.DeployInbox(
+		auth,
+		blockchain,
+		"Hello World",
+	)
+
+	fmt.Printf("Contract pending deploy: 0x%x\n", address)
+}
